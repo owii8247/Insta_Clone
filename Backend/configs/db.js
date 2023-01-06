d1cf48d40cbcc81f241f42db272c714ee620a28a
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2023-01-06 10:43:22 +0530

    Check

--- Backend/configs/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-require('dotenv').config()
-//console.log(process.env.MONGO_URL)
-const connection = mongoose.connect(process.env.MONGO_URL)
-
-module.exports = connection
